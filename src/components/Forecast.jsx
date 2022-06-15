e186751c3c130877232f6f3befe232f182fe5936
Geoffrey Mwangi <gmwangi3174@gmail.com>
2022-06-15 12:08:43 +0300

    connected the api with the frontend

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { iconUrlFromCode } from "../services/WeatherService";
 
-const Forecast = ({ title }) => {
+const Forecast = ({ title, items }) => {
   return (
     <div>
       <div className="flex items-center justify-start my-6">
@@ -8,46 +9,13 @@ const Forecast = ({ title }) => {
       </div>
       <hr className="my-2" />
       <div className="flex flex-row items-center justify-between text-white">
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30PM</p>
-          <img
-            src="https://thumbs.dreamstime.com/z/cartoon-sun-holding-cloud-17925535.jpg"
-            className="w-12 my-1"
-          ></img>
-          <p className="font-medium">22&#8451;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30PM</p>
-          <img
-            src="https://thumbs.dreamstime.com/z/cartoon-sun-holding-cloud-17925535.jpg"
-            className="w-12 my-1"
-          ></img>
-          <p className="font-medium">22&#8451;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30PM</p>
-          <img
-            src="https://thumbs.dreamstime.com/z/cartoon-sun-holding-cloud-17925535.jpg"
-            className="w-12 my-1"
-          ></img>
-          <p className="font-medium">22&#8451;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30PM</p>
-          <img
-            src="https://thumbs.dreamstime.com/z/cartoon-sun-holding-cloud-17925535.jpg"
-            className="w-12 my-1"
-          ></img>
-          <p className="font-medium">22&#8451;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30PM</p>
-          <img
-            src="https://thumbs.dreamstime.com/z/cartoon-sun-holding-cloud-17925535.jpg"
-            className="w-12 my-1"
-          ></img>
-          <p className="font-medium">22&#8451;</p>
-        </div>
+        {items.map((item) => (
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">{item.title}</p>
+            <img src={iconUrlFromCode(item.icon)} className="w-12 my-1"></img>
+            <p className="font-medium">{`${item.temp}°`}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TopButtons = () => {
+const TopButtons = ({ setQuery }) => {
   const cities = [
     {
       id: 1,
@@ -19,14 +19,18 @@ const TopButtons = () => {
       title: "Toronto",
     },
     {
-      id: 1,
+      id: 5,
       title: "Paris",
     },
   ];
   return (
     <div className="flex items-center justify-around my-6">
       {cities.map((city) => (
-        <button key={city.id} className="text-white text-lg font-medium">
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
           {city.title}
         </button>
       ))}
